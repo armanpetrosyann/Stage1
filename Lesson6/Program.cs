d349e64040db6f454d8bd5e84e88ac9ba29a4ac7
arman.p <arman.p@F4RPDEV-62>
2022-07-11 09:50:28 +0400

    Added Mrtod 2.Using Iterative way

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -8,7 +8,7 @@ namespace Lesson6
         {
             // Sort array numbers by descending
 
-            // Method 1. Using Array.Sort() and Array.Reverse() Method
+            // Method 1.using Array.Sort() and Array.Reverse() Method
 
             int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
 
@@ -19,6 +19,29 @@ namespace Lesson6
             foreach (int item in arr)
             {
                 Console.WriteLine(item + " ");
+            }
+
+            // Method 2. Using Iterative way
+
+            int[] array = new int[] { 1, 9, 6, 7, 5, 9 };
+
+            int temp;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            foreach (int value in array)
+            {
+                Console.WriteLine(value + " ");
             }
         }
     }
