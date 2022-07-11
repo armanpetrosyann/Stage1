09271c70890830b64364b90c40ac2c8b24e761e1
arman.p <arman.p@F4RPDEV-62>
2022-07-11 09:37:09 +0400

    Added program . Sort array numbers by descending Method 1. Using Array.Sort() and Array.Reverse() Method

---
+++ Lesson6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sort array numbers by descending
+
+            // Method 1. Using Array.Sort() and Array.Reverse() Method
+
+            int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
+
+            Array.Sort(arr);
+
+            Array.Reverse(arr);
+
+            foreach (int item in arr)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
