df228db0446bc994ef81c537000be67df3e65038
arman.p <arman.p@F4RPDEV-62>
2022-07-11 11:48:53 +0400

    Added Execrise.  Add random numbers in List<int>() and sort

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -1,61 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Lesson6
 {
     internal class Program
     {
+
+
+
         static void Main(string[] args)
         {
             // Sort array numbers by descending
 
             // Method 1.using Array.Sort() and Array.Reverse() Method
 
-            int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
+            //int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
 
-            Array.Sort(arr);
+            //Array.Sort(arr);
 
-            Array.Reverse(arr);
+            //// Array.Reverse(arr);
 
-            foreach (int item in arr)
-            {
-                Console.WriteLine(item + " ");
-            }
+            //foreach (int item in arr)
+            //{
+            //    Console.WriteLine(item + " ");
+            //}
 
-            // Method 2. Using Iterative way
+            //// Method 2. Using Iterative way
 
-            int[] array = new int[] { 10, 30, 20, 40, 60, 50 };
+            //int[] array = new int[] { 10, 30, 20, 40, 60, 50 };
 
-            int temp;
+            //int temp;
 
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (array[i] < array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-            foreach (int value in array)
-            {
-                Console.WriteLine(value + " ");
-            }
+            //for (int i = 0; i <= array.Length - 1; i++)
+            //{
+            //    for (int j = 0; j < array.Length; j++)
+            //    {
+            //        if (array[i] < array[j])
+            //        {
+            //            temp = array[i];//10
+            //            array[i] = array[j];//10=30
+            //            array[j] = temp;//30=10
+            //        }
+            //    }
+            //}
+            //foreach (int value in array)
+            //{
+            //    Console.WriteLine(value + " ");
+            //}
+
 
             // Method 3. Using LINQ descending
 
-            int[] myArr = new int[] { 1, 9, 6, 7, 5, 9 };
+            //int[] myArr = new int[] { 1, 9, 6, 7, 5, 9 };
 
-            myArr = myArr.OrderByDescending(c => c).ToArray();
+            //myArr = myArr.OrderByDescending(c => c).ToArray();
 
-            foreach (int value in myArr)
-            {
-                Console.WriteLine(value + " ");
-            }
-           
+            //foreach (int value in myArr)
+            //{
+            //    Console.WriteLine(value + " ");
+            //}
+
+            //var numbers = new List<int>() { 1, 9, 6, 7, 5, 9,9,5,6,99 };
+            //numbers.OrderBy(a => a).Distinct().ToList().ForEach(a => Console.WriteLine(a));
+
+            // Exercise 4. Add random numbers in List<int>() and sort 
+
+            //var list = new List<int>();
+            //Random random = new Random();
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    list.Add(random.Next(0, 100));
+            //}
+
+            //foreach (var item in list.Distinct().OrderBy(a => a))
+            //{
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
