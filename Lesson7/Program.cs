0f5e2f94bd7cfd764f66b1a2d7b98430e884adc4
arman.p <arman.p@F4RPDEV-62>
2022-07-12 10:43:30 +0400

    Added program. 3.Write a program in C# Sharp to print individual characters of the string in reverse order.

--- Lesson7/Program.cs
+++ Lesson7/Program.cs
@@ -4,6 +4,22 @@ namespace Lesson7
 {
     internal class Program
     {
+        // Method to reverse string entered from console 
+
+        public static string Reverse(string Input)
+        {
+            char[] charArray = Input.ToCharArray();
+
+            string reversedString = String.Empty;
+
+            for (int i = charArray.Length - 1; i > -1; i--)
+            {
+                reversedString += charArray[i];
+            }
+
+            return reversedString;
+        }
+
         static void Main(string[] args)
         {
             // 1.Write a program in C# Sharp to separate the individual characters from a string. 
@@ -13,6 +29,8 @@ namespace Lesson7
             Console.Write("Input the string: ");
 
             string askForString = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Expected output: ");
 
             for (int i = 0; i < askForString.Length; i++)
             {
@@ -33,9 +51,21 @@ namespace Lesson7
             {
                 checker++;
             }
+            Console.WriteLine("Expected output: ");
+
             Console.WriteLine($"Total number of letter in string is: {checker}");
 
-            
+            // 3.Write a program in C# Sharp to print individual characters of the string in reverse order. 
+
+            Console.WriteLine("Test Data");
+
+            Console.Write("Input the string: ");
+
+            string askForText = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Expected output: ");
+
+            Console.WriteLine(Reverse(askForText));
         }
     }
 }
