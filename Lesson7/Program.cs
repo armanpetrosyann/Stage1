3dfc220ab57933de01b2e9e24ace40e8e5abd49d
arman.p <arman.p@F4RPDEV-62>
2022-07-12 10:26:57 +0400

    Added program. 1.Write a program in C# Sharp to separate the individual characters from a string.

---
+++ Lesson7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lesson7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Write a program in C# Sharp to separate the individual characters from a string. 
+
+            Console.WriteLine("Test Data:");
+
+            Console.Write("Input the string: ");
+
+            string askForString = Convert.ToString(Console.ReadLine());
+
+            for (int i = 0; i < askForString.Length; i++)
+            {
+                Console.Write(askForString[i] + " ");
+            }
+ 
+           
+        }
+    }
+}
