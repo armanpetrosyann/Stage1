ad1e6820e7b78bcddd4a1c16ac34f1211a4ac918
arman.p <arman.p@F4RPDEV-62>
2022-07-20 15:33:56 +0400

    Added found [ErrorMessage] by number from console.

---
+++ errorMessages/Errors.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace errorMessages
+{
+    public enum ErrorMessages
+    {
+        INCORRECT_DATA,
+        INCORRECT_ERROR,
+        INCORRECT_MESSAGE,
+        INCORRECT_EXCEPTION,
+        INCORRECT_STACK,
+        INCORRECT_STACK_ERROR
+    }
+}
---
+++ errorMessages/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace errorMessages
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number to get error message: ");
+
+            var askForMessageNumber = Convert.ToInt32(Console.ReadLine());
+
+
+
+            switch (askForMessageNumber)
+            {
+                case 0:
+                    Console.WriteLine(ErrorMessages.INCORRECT_DATA);
+                    break;
+                case 1:
+                    Console.WriteLine(ErrorMessages.INCORRECT_ERROR);
+                    break;
+                case 2:
+                    Console.WriteLine(ErrorMessages.INCORRECT_MESSAGE);
+                    break;
+                case 3:
+                    Console.WriteLine(ErrorMessages.INCORRECT_EXCEPTION);
+                    break;
+                    case 4:
+                    Console.WriteLine(ErrorMessages.INCORRECT_STACK);
+                    break;
+                case 5:
+                    Console.WriteLine(ErrorMessages.INCORRECT_STACK_ERROR);
+                    break;
+                default:
+                    Console.WriteLine("Error message doesn't found");
+                    break;
+            }
+
+        }
+    }
+}
